e75465e39e2e1b04eae9d1bc0731d4b1f42e4b6f
sgu0927 <mylazysong@naver.com>
2023-04-19 15:24:50 +0900

    feat(#1) - api server redis 설정 수정

--- api-server/src/main/java/com/jpshoppingmall/config/RedisCacheConfig.java
+++ api-server/src/main/java/com/jpshoppingmall/config/RedisCacheConfig.java
@@ -3,6 +3,8 @@ package com.jpshoppingmall.config;
 import com.jpshoppingmall.util.PurchaseCountCacheKeyGenerator;
 import java.time.Duration;
 import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,19 +12,38 @@ import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisCacheConfig {
+
+    @Value("${spring.redis.cache.host}")
+    private String host;
+    @Value("${spring.redis.cache.port}")
+    private int port;
+
+    @Bean(name = "redisCacheConnectionFactory")
+    public RedisConnectionFactory redisCacheConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration
+            = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    public RedisCacheManager cacheManager(
+        @Qualifier("redisCacheConnectionFactory") RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
             .disableCachingNullValues()
             .entryTtl(Duration.ofSeconds(10))   // 기본 TTL
             .computePrefixWith(CacheKeyPrefix.simple())
             .serializeKeysWith(
-                RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    new StringRedisSerializer())
             );
 
         HashMap<String, RedisCacheConfiguration> configMap = new HashMap<>();
